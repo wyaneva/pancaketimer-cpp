c83b51fe0dc31b96228e9615b2e9b3a817ccfbaf
Vanya Yaneva <wyaneva@gmail.com>
2020-04-21 16:52:27 +0100

    Separated timer into own function

--- src/pancaketimer.cpp
+++ src/pancaketimer.cpp
@@ -11,35 +11,34 @@ class PancakeTimer {};
 
 int const time_step = 1;
 
-int time_pancake(int const side1, int const side2, int const flip) {
+void run_timer(int const time, bool const do_print) {
+  int threshold = time_step;
+  int bar_fill = time;
 
-  for (int i = 1; i <= 2; i++) {
-    int threshold = time_step;
-    int bar_fill = side1;
-    if (i == 2) {
-      bar_fill = side2;
-    }
-
-    auto start = std::chrono::high_resolution_clock::now();
-    while (true) {
-      auto end = std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double> tt = end - start;
-      auto t = std::chrono::duration_cast<std::chrono::seconds>(tt);
-      if (t.count() > threshold) {
-        threshold += time_step;
+  auto start = std::chrono::high_resolution_clock::now();
+  while (true) {
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> tt = end - start;
+    auto t = std::chrono::duration_cast<std::chrono::seconds>(tt);
+    if (t.count() > threshold) {
+      threshold += time_step;
+      if (do_print)
         std::cout << bar_fill << std::endl;
-        bar_fill -= time_step;
-      }
-
-      if (bar_fill < 0) {
-        break;
-      }
+      bar_fill -= time_step;
     }
 
-    if (i == 1) {
-      std::cout << "FLIP!" << std::endl;
+    if (bar_fill < 0) {
+      break;
     }
   }
+}
+
+int time_pancake(int const side1, int const side2, int const flip) {
+
+  run_timer(side1, true);
+  std::cout << "FLIP!" << std::endl;
+  run_timer(flip, false);
+  run_timer(side2, true);
 
   return 0;
 }
