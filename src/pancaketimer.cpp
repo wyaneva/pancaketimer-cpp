c32c974947b72523acfb205451bcff43cfce8bb9
Vanya Yaneva <wyaneva@gmail.com>
2020-04-25 16:47:43 +0100

    Printing the time in the progress bar

--- src/pancaketimer.cpp
+++ src/pancaketimer.cpp
@@ -4,30 +4,49 @@
  */
 
 #include "pancaketimer.h"
-#include <iostream>
+#include <algorithm>
 #include <chrono>
+#include <iostream>
+#include <string>
 
 class PancakeTimer {};
 
-int const time_step = 1;
-int const total_width = 100;
+int const time_step = 1;     // seconds
+int const total_width = 100; // columns
 char const BAR_CHAR = '=';
 char const SPACE_CHAR = ' ';
 
-void write_progress(int bar_fill, int total_width, std::string text) {
-  std::string fill = "[";
+void write_progress(int bar_width, int total_width, std::string text) {
+  // calculate spacing for the text in the progress bar
+  auto text_len = text.size();
+  auto half_width = total_width / 2;
+  auto half_text_len_lhs = text_len / 2;
+  auto half_text_len_rhs = text_len - half_text_len_lhs;
+  int const bar_lhs_const = half_width - half_text_len_lhs;
+  auto bar_lhs = std::min(bar_lhs_const, bar_width);
+  int const bar_rhs_const = bar_width - half_width - half_text_len_rhs;
+  auto bar_rhs = std::max(0, bar_rhs_const);
+  auto spacing_lhs = half_width - half_text_len_lhs - bar_lhs;
+  auto spacing_rhs = total_width - bar_rhs - half_width - half_text_len_rhs;
 
-  // fill characters
-  for (int i = 0; i < bar_fill; i++) {
+  // compose the progress bar
+  std::string fill = "[";
+  for (int i = 0; i < bar_lhs; i++) {
     fill += BAR_CHAR;
   }
-
-  // fill space
-  for (int i = bar_fill; i < total_width; i++) {
+  for (int i = 0; i < spacing_lhs; i++) {
+    fill += SPACE_CHAR;
+  }
+  fill += text;
+  for (int i = 0; i < bar_rhs; i++) {
+    fill += BAR_CHAR;
+  }
+  for (int i = 0; i < spacing_rhs; i++) {
     fill += SPACE_CHAR;
   }
   fill += "]";
 
+  // write the progress bar on the console
   std::cout << fill << std::endl;
 }
 
@@ -43,8 +62,11 @@ void run_timer(int const time, bool const do_print) {
     auto t = std::chrono::duration_cast<std::chrono::seconds>(tt);
     if (t.count() > threshold) {
       threshold += time_step;
-      if (do_print)
-        write_progress(bar_fill, total_width, "");
+      if (do_print) {
+        auto text =
+            std::to_string(time - t.count() + 2) + "/" + std::to_string(time);
+        write_progress(bar_fill, total_width, text);
+      }
       bar_fill -= step;
     }
 
