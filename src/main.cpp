4fbe56d7d809ebdd9438533a61bf9b9c3bd14a04
Vanya Yaneva <wyaneva@gmail.com>
2020-04-30 20:35:49 +0100

    Added print for default modes.

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,25 @@
 #include <iostream>
 #include <vector>
 
+// TODO 1: Function to print accepted arguments -h
+// TODO 2: Add repeat count argument
+// TODO 3: Add side arguments
+// TODO 4: Add sound
+
 Mode const testmode{"test", 5, 5, 5};
 Mode const pancake{"pancake", 85, 60, 5};
 Mode const crepe{"crepe", 80, 40, 10};
 Mode const emptyMode{"empty", 0, 0, 0};
 
 std::vector<Mode> defaultModes{pancake, crepe};
+
+void printDefaultModes() {
+  std::cout << "Available modes are: ";
+  for (auto mode : defaultModes) {
+    std::cout << mode.name << " ";
+  }
+  std::cout << std::endl;
+}
 
 Mode const findModeByName(std::string const &modeName) {
   for (auto mode : defaultModes) {
@@ -26,20 +39,22 @@ Mode const findModeByName(std::string const &modeName) {
 }
 
 int main(int argc, const char *argv[]) {
-  std::cout << "Time for pancakes!" << std::endl;
   Mode mode = pancake;
 
   ArgumentParser parser(argc, argv);
-  if(parser.doesArgumentExist("--mode")) {
+  if (parser.doesArgumentExist("--mode")) {
     auto modeName = parser.getArgumentValue("--mode");
     mode = findModeByName(modeName);
+
+    if (mode.name == emptyMode.name) {
+      std::cout << modeName << " is an invalid mode." << std::endl
+                << "Please, provide a valid mode." << std::endl;
+      printDefaultModes();
+      return 0;
+    }
   }
 
-  if (mode.name == emptyMode.name) {
-    std::cout << "Please, provide a valid mode." << std::endl;
-    return 0;
-  }
-
+  std::cout << "Time for pancakes!" << std::endl;
   time_pancake(mode);
   return 0;
 }
