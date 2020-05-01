ab7ea758eaeff684e31bbff261d5dcb6c556c7b3
Vanya Yaneva <wyaneva@gmail.com>
2020-05-01 20:49:24 +0100

    Added repeats argument

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,12 @@
 #include <iostream>
 #include <vector>
 
-// TODO 1: Function to print accepted arguments -h
-// TODO 2: Add repeat count argument
-// TODO 3: Add side arguments
+// TODO 1: Add repeat count argument; fix cin.ignore()
+// TODO 2: Add side arguments
+// TODO 3: Function to print accepted arguments -h
 // TODO 4: Add sound
 
-Mode const testmode{"test", 5, 5, 5};
+Mode const testmode{"test", 5, 5, 2};
 Mode const pancake{"pancake", 85, 60, 5};
 Mode const crepe{"crepe", 80, 40, 10};
 Mode const emptyMode{"empty", 0, 0, 0};
@@ -39,9 +39,9 @@ Mode const findModeByName(std::string const &modeName) {
 }
 
 int main(int argc, const char *argv[]) {
-  Mode mode = pancake;
-
   ArgumentParser parser(argc, argv);
+
+  Mode mode = pancake;
   if (parser.doesArgumentExist("--mode")) {
     auto modeName = parser.getArgumentValue("--mode");
     mode = findModeByName(modeName);
@@ -54,7 +54,21 @@ int main(int argc, const char *argv[]) {
     }
   }
 
+  int repeats = 1;
+  if (parser.doesArgumentExist("--repeats")) {
+    auto repeatsstr = parser.getArgumentValue("--repeats");
+
+    // TODO: handle std::invalid_argument
+    repeats = std::stoi(repeatsstr);
+  }
+
   std::cout << "Time for pancakes!" << std::endl;
-  time_pancake(mode);
+  for (int i = 0; i < repeats; i++) {
+    time_pancake(mode);
+    if (i < repeats - 1) {
+      std::cout << "Press Enter to countinue..."<< "\r" << std::flush;
+      std::cin.ignore();
+    }
+  }
   return 0;
 }
